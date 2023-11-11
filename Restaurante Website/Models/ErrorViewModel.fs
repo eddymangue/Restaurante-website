0df7818b1c25daeff430b2696162bdbe731e58b8
eddymangue <eddymangue005@gmail.com>
2023-11-11 15:13:41 +0200

    Fist commit

---
+++ Restaurante Website/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace Restaurante_Website.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
